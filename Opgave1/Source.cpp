febd88a3042096edfc9e2c1928213d1210e7cddc
Ehabben10 <202107666@post.au.dk>
2024-02-14 11:28:50 +0100

    ucomment code

--- Opgave1/Source.cpp
+++ Opgave1/Source.cpp
@@ -10,19 +10,19 @@ int main()
 
     sa.print();
 
-    // sa.push(2);
-    // sa.print();
+    sa.push(2);
+    sa.print();
 
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     sa.push(i);
-    // }
-    // sa.print();
+    for (int i = 0; i < 10; i++)
+    {
+        sa.push(i);
+    }
+    sa.print();
 
-    // cout << "Pop: " << sa.pop() << endl;
-    // cout << "Pop: " << sa.pop() << endl;
-    // cout << "Pop: " << sa.pop() << endl;
-    // cout << "Pop: " << sa.pop() << endl;
-    // sa.print();
+    cout << "Pop: " << sa.pop() << endl;
+    cout << "Pop: " << sa.pop() << endl;
+    cout << "Pop: " << sa.pop() << endl;
+    cout << "Pop: " << sa.pop() << endl;
+    sa.print();
     return 0;
 }
